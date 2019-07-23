6227e0b9779ea937e2ab1e6cdea9f1bbed0dfd2c
2228787638 <2228787638@qq.com>
2019-07-23 22:59:05 +0800

    [feat: implement click the back button show the comfirm dialog]

--- src/App.vue
+++ src/App.vue
@@ -14,13 +14,5 @@ export default {
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
 
-  display:block;
-  color: #2c3e50;
-}
 </style>
--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,5 +1,6 @@
 <template>
     <div>
+        <button @click="goback">返回</button>
         <Inputs></Inputs>
         <Item></Item>
     </div>
@@ -10,7 +11,17 @@
     import Item from "./Item";
     export default {
         name: "index",
-        components: {Inputs,Item}
+        components: {Inputs,Item},
+        methods:{
+            goback(){
+                const answer = window.confirm('确认返回？');
+        if(answer){
+                    this.$router.push({path: '/'});
+                }else{
+
+                }
+            }
+        }
     }
 </script>
 
